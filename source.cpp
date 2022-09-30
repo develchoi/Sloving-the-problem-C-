932e041228752ce742e57092f2c6dfb5a795580f
boy_happy <boy_happy1@naver.com>
2022-09-30 10:15:34 +0900

    Dynamic Programing(Fibo)

---
+++ source.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+//Dynamic Programing
+//How to solve: Bottom-up
+
+int Fibo(int num) {
+	int* a = new int[num];
+
+	for (int i = 0; i < num; i++) {
+		if (i == 0 || i == 1) a[i] = 1;
+		else
+			a[i] = a[i - 1] + a[i - 2];
+	}
+
+	return a[num - 1];
+}
+
+
+int main() {
+	int n;
+	cin >> n;
+
+	int ans = Fibo(n);
+	cout << ans;
+}
+
+//What is difference between DP and Divide-Conqure 
